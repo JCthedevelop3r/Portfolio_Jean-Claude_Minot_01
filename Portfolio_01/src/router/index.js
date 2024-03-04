c6969e1991a96273935a46cd3e57a74d3a40c35a
JIICE <minotjeanclaude60@gmail.com>
2024-03-04 19:27:37 +0100

    Create views, routes and router-view tag

--- Portfolio_01/src/router/index.js
+++ Portfolio_01/src/router/index.js
@@ -1,21 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import homepage from "../views/homepage.vue"
+import figma from "../views/figma.vue"
+import error404 from "../views/error404.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "Homepage",
+      component: homepage,
     },
+
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: "/figma",
+      name: "figma",
+      component: figma,
+    },
+
+    {
+      path: "/.catchAll(.*)",
+      name: "error404",
+      component: error404,
     }
   ]
 })
