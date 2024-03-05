8d0d1045fcbc36147b373c022e2e2f3b9d3fe9f5
JIICE <minotjeanclaude60@gmail.com>
2024-03-05 23:21:39 +0100

    Code the footer with HTML and CSS

---
+++ Portfolio_01/src/components/footerComponent.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+    <footer>
+        <section>
+            <img src="../assets/Images/Logo_de_Github.png" alt="Logo de Github">
+            <img src="../assets/Images/Logo_de_X.png" alt="Logo de X, ex-Twitter">
+            <img src="../assets/Images/Logo_de_Linkedin.png" alt="Logo de Linkedin">
+        </section>
+
+        <small><p>Dernière mise à jour du site le xx mars 2024.</p></small>
+    </footer>
+</template>
+
+<style scoped>
+    footer {
+        display: flex;
+        justify-content: space-between;
+        margin-top: 80px;
+        padding-bottom: 10px;
+    }
+
+    section {
+        width: 184px;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    p {
+        font-family: "Assistant", sans-serif;
+        font-weight: 300;
+    }
+</style>
--- Portfolio_01/src/views/homepage.vue
+++ Portfolio_01/src/views/homepage.vue
@@ -1,10 +1,12 @@
 <script setup>
     import { RouterLink, RouterView } from 'vue-router';
     import headerComponent from '@/components/headerComponent.vue';
+    import footerComponent from '@/components/footerComponent.vue';
 </script>
 
 <template>
     <headerComponent></headerComponent>
+    <footerComponent></footerComponent>
 </template>
 
 <style scoped>
