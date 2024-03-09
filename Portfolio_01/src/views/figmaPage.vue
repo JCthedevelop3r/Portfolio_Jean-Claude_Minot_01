51ddc5647a06f1a6bef5ecddc4d0471af0bb07c1
JIICE <minotjeanclaude60@gmail.com>
2024-03-09 01:24:48 +0100

    Start to create the Figma page

--- Portfolio_01/src/views/figmaPage.vue
+++ Portfolio_01/src/views/figmaPage.vue
@@ -1,9 +1,37 @@
 <script setup>
-
+    import headerComponent from "@/components/headerComponent.vue";
+    import footerComponent from "@/components/footerComponent.vue";
 </script>
 
 <template>
-
+    <headerComponent></headerComponent>
+    <h2>Wireframes desktop</h2>
+    <h3>Page d'accueil</h3>
+    <img src="#" alt="#">
+    <h3>Page Produit</h3>
+    <img src="#" alt="#">
+    <h3>Page Commande</h3>
+    <img src="#" alt="#">
+    <h3>Page à propos</h3>
+    <img src="#" alt="#">
+    <h3>Page assistance</h3>
+    <img src="#" alt="#">
+    <h2>Wireframes mobile</h2>
+    <h3>Page d'accueil</h3>
+    <img src="#" alt="#">
+    <h3>Page Produit</h3>
+    <img src="#" alt="#">
+    <h3>Page Commande</h3>
+    <img src="#" alt="#">
+    <h3>Page à propos</h3>
+    <img src="#" alt="#">
+    <h3>Page assistance</h3>
+    <img src="#" alt="#">
+    <h2>Prototype desktop</h2>
+    <img src="#" alt="#">
+    <h2>Prototype mobile</h2>
+    <img src="#" alt="#">
+    <footerComponent></footerComponent>
 </template>
 
 <style scoped>
